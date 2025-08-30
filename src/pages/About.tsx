dca8407248736fcffb737692d654bc8255c999eb
AlfredoJSpera <alfredo.j.spera@gmail.com>
2025-08-30 18:29:39 +0200

    Display current route pathname on About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,16 @@
 import ChangePage from "../components/ChangePage";
 import ThemeButton from "../components/ThemeButton";
+import { useLocation } from "react-router-dom";
 
 function About() {
+	const location = useLocation();
+
 	return (
 		<div className="app-container">
 			<h1>This is an About page</h1>
+
+			<p>Route pathname: {location.pathname}</p>
+
 			<ChangePage />
 			<ThemeButton />
 		</div>
