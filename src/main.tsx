a86030245cafa3e91d59976e3c4be847bdf46c65
AlfredoJSpera <alfredo.j.spera@gmail.com>
2025-08-30 17:28:30 +0200

    Add a page not found route

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,10 @@ import App from "./App.tsx";
 import { ThemeProvider } from "./theme/ThemeProvider.tsx";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
-// Use BrowserRouter if it's for a regular website
-// Use HashRouter if it's for an extension
+// Use Browser Router if it's for a regular website
+// Use Hash Router if it's for an extension
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<HashRouter>
@@ -14,6 +15,8 @@ createRoot(document.getElementById("root")!).render(
 				<Routes>
 					<Route path="/" element={<App />} />
 					<Route path="/about" element={<About />} />
+					{/* Catch-all route for not found pages */}
+					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</ThemeProvider>
 		</HashRouter>
